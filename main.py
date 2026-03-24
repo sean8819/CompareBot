b8fe1a30f089ce35776d3ffc6348e5e1dfbaf5e2
Giovanni Gulisano <ghignoxviii@hotmail.it>
2026-03-24 23:33:00 +0100

    feat: Add download button UI

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-from src.handlers import about, beauty, download, service, start
+from src.handlers import about, beauty, download, service, start, handle_resolution
 from src.logo import print_logo
+
+from telegram.ext import CallbackQueryHandler
+from src.handlers import handle_buttons
 
 
 def main():
@@ -17,6 +20,9 @@ def main():
     app.add_handler(CommandHandler("about", about))
     app.add_handler(CommandHandler("service", service))
     app.add_handler(CommandHandler("beauty", beauty))
+    app.add_handler(CallbackQueryHandler(handle_buttons, pattern="^(audio|video|annulla)$"))
+    app.add_handler(CallbackQueryHandler(handle_resolution, pattern="^(360|480|720|annulla)$"))
+
 
     print_logo()
     print("Bot avviato correttamente...\nIn attesa di messagg...!")
